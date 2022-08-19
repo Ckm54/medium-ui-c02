b93bb9073f7bd712d7561817249df6e3611a10f5
collo <mutaic84@gmail.com>
2022-08-19 16:11:17 +0300

    update styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -36,11 +36,18 @@ export default function Home({posts}: Props ) {
 
       {/* posts */}
 
-      <div>
+      <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 md:gap-6 p-2 md:p-6'>
         {posts.map((post) => (
           <Link key={post._id} href={`/posts/${post.slug.current}`}>
-            <div>
-              <img src={urlFor(post.mainImage).url()} alt="" />
+            <div className='border rounded-lg group cursor-pointer overflow-hidden'>
+              <img className='w-full h-60 object-cover group-hover:scale-105 transition-transform duration-200 ease-in-out' src={urlFor(post.mainImage).url()} alt={post.title} />
+              <div className='flex justify-between p-5 bg-white'>
+                <div>
+                  <p className='text-lg font-bold'>{post.title}</p>
+                  <p className='text-xs'>{post.description} by {post.author.name}</p>
+                </div>
+                <img className='h-12 w-12 rounded-full object-cover' src={urlFor(post.author.image).url()} alt="" />
+              </div>
             </div>
           </Link>
         ))}
